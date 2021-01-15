d7246cf1564215530d1fc116eca7071a3cf611b9
Derek-94 <ghdtjrrl94@gmail.com>
2021-01-15 16:44:40 +0900

    feat(path): Update path finding func.
    - Update path finding function.
    - Register 3 constant values and 3 messages that alert exceptional case.

--- path/findPath.js
+++ path/findPath.js
@@ -1,9 +1,11 @@
 import {message} from "../constantValues/message.js";
 import {constants} from "../constantValues/constants.js";
+import Dijkstra from "../src/utils/Dijkstra.js";
 
 const findPathPreliminaryWork = () => {
     const departureStation = document.getElementById("departure-station-name-input").value;
     const arrivalStation = document.getElementById("arrival-station-name-input").value;
+    const findPathResultTable = document.getElementById("find-path-result");
 
     // 1. localStorage - station에 두 역이 있는지..
     const localStorageStationData = localStorage.getItem(constants.LOCAL_STORAGE_KEY_STATION);
@@ -15,11 +17,63 @@ const findPathPreliminaryWork = () => {
         return ;
     }
     
-    // 2. 갈 수는 있는건지.
+    // 2. 두 개의 역의 이름이 같은지
+    if(departureStation === arrivalStation) {
+        alert(message.FIND_PATH_DEPARTURE_ARRIVAL_SAME);
+        return;
+    }
+    findPath(departureStation, arrivalStation);
+    findPathResultTable.classList.remove("hidden");
 }
 
-const findPath = () => {
+const findPath = (departureStation, arrivalStation) => {
+    const result = runDijkstra(departureStation, arrivalStation);
+    if(result === undefined) {
+        alert(message.FIND_PATH_NOT_CONNECTED);
+        return;
+    }
+    const [distance, duration] = calculateDistanceDuration(result);
+    printFoundPath(result, distance, duration);
+}
 
+const runDijkstra = (start, goal) => {
+    const dijkstra = new Dijkstra();
+
+    const localStorageAllLineData = localStorage.getItem(constants.LOCAL_STORAGE_KEY_ALLLINE);
+    const localStorageAllLineDataArray = JSON.parse(localStorageAllLineData);
+    const optionValue = document.getElementsByName("search-type");
+
+    localStorageAllLineDataArray.forEach(lineInfo => {
+        const localStorageEachLineData = localStorage.getItem(lineInfo.line);
+        const localStorageEachLineDataArray = JSON.parse(localStorageEachLineData);
+        for(let i = 0 ; i < localStorageEachLineDataArray.length - 1 ; i++) {
+            optionValue[0].checked ? dijkstra.addEdge(localStorageEachLineDataArray[i].station, localStorageEachLineDataArray[i + 1].station, localStorageEachLineDataArray[i].distance) : dijkstra.addEdge(localStorageEachLineDataArray[i].station, localStorageEachLineDataArray[i + 1].station, localStorageEachLineDataArray[i].duration)
+        }
+    })
+    return dijkstra.findShortestPath(start, goal);
+}
+
+const calculateDistanceDuration = (result) => {
+    let totalDistance = 0;
+    let totalDuration = 0;
+    const localStorageAllSectionData = localStorage.getItem(constants.LOCAL_STORAGE_KEY_SECTION);
+    const localStorageAllSectionDataArray = JSON.parse(localStorageAllSectionData);
+
+    for(let i = 0 ; i < result.length - 1 ; i++) {
+        const path = result[i].concat(result[i + 1]);
+        totalDistance += localStorageAllSectionDataArray.find(sectionInfo => sectionInfo.section === path).distance;
+        totalDuration += localStorageAllSectionDataArray.find(sectionInfo => sectionInfo.section === path).duration;
+    }
+    return [totalDistance, totalDuration];
+}
+
+const printFoundPath = (result, distance, duration) => {
+    const pathFoundTable = document.querySelector("#find-path-result-table tbody");
+    pathFoundTable.innerHTML = `<tr><th>총 거리</th><th>총 소요시간</th></tr>`;
+    let tableContents = `<tr><td>${distance}km</td><td>${duration}분</td></tr><tr><td colspan = "2">`;
+    result.forEach((station, index) => { (index === result.length - 1) ? tableContents += `${station}` : tableContents += `${station} ➡` })
+    tableContents += `</td></tr>`;
+    pathFoundTable.innerHTML += tableContents;
 }
 
 export {findPathPreliminaryWork};
