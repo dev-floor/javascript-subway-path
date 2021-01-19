3aa4c74647310a867db58a5cfbd392767f7b5182
JeongPyo <jeongpyo_park@tmax.co.kr>
2021-01-19 19:48:47 +0900

    feat(line): 지하철 노선 목록 view 구현

--- src/lineManager.js
+++ src/lineManager.js
@@ -1,20 +1,41 @@
 import {LINE_MANAGER_PAGE_TEMPLATE} from './view/lineTemplate.js';
 import {stationStorage} from './storage/stationStorage.js'
+import lineStorage from './storage/lineStorage.js';
 
 const fillSelectBoxTag = (selectBoxTag) => {
   let stations = stationStorage().getStation();
   stations.forEach((station) => {
     selectBoxTag.innerHTML += `<option>${station}</option>`;
   })
-};
+}
 
 const nameDuplicateCheck = (lineName) => {
 
-};
+}
 
 const addLineToStorage = (lineName) => {
 
-};
+}
+
+const fillLineTableBody = (lineTableBodyTag) => {
+  let lines = lineStorage().getLine();
+  
+  lines.forEach((line) => {
+    console.log(line.stations[0].length)
+    let startStation = line.stations[0][0];
+    let endStation = line.stations[0][line.stations[0].length-1];
+    let tbodyRow = lineTableBodyTag.insertRow(lineTableBodyTag.rows.length);
+    let firstCell = tbodyRow.insertCell(0);
+    let secondCell = tbodyRow.insertCell(1);
+    let thirdCell = tbodyRow.insertCell(2);
+    let fourthCell = tbodyRow.insertCell(3);
+
+    firstCell.innerHTML = line.name;
+    secondCell.innerHTML = startStation;
+    thirdCell.innerHTML = endStation;
+    fourthCell.innerHTML = '<button class="line-delete-button" data-name=' + line.name + '>삭제</button>';
+  })
+}
 
 export default function lineManagerPage(contentSectionTag) {
   contentSectionTag.innerHTML = LINE_MANAGER_PAGE_TEMPLATE;
@@ -23,6 +44,9 @@ export default function lineManagerPage(contentSectionTag) {
   let lineStartStationSelector = document.getElementById('line-start-station-selector');
   let lineEndStationSelector = document.getElementById('line-end-station-selector');
   let lineAddButton = document.getElementById('line-add-button');
+  let lineTableBodyTag = document.getElementsByClassName('line-table-tbody')[0];
+  console.log(lineTableBodyTag)
+  fillLineTableBody(lineTableBodyTag);
   
   const addLineClickHandler = () => {
     let lineName = lineNameInput.value;
