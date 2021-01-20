e52d526a06c7ceeafa1b6b8027dbdc31c37187ac
JeongP <wjdvy0409@naver.com>
2021-01-20 22:57:20 +0900

    feat(section): 레이아웃 구현

--- src/sectionManager.js
+++ src/sectionManager.js
@@ -1,6 +1,6 @@
-import { Linter } from 'eslint';
 import lineStorage from './storage/lineStorage.js';
-import {SECTION_MANAGER_PAGE_TEMPLATE, SECTION_REGISTER_FORM_TEMPLATE} from './view/sectionTemplate.js'
+import stationStorage from './storage/stationStorage.js';
+import {SECTION_MANAGER_PAGE_TEMPLATE, SECTION_REGISTER_FORM_TEMPLATE, SECTION_TABLE_BODY} from './view/sectionTemplate.js'
 
 const makeLineButtonView = (lines) => {
   let lineButtonTemplate = '';
@@ -8,6 +8,40 @@ const makeLineButtonView = (lines) => {
     lineButtonTemplate = lineButtonTemplate + `<button class="section-line-menu-button" data-lineName=${line.name}>${line.name}</button> &nbsp`;
   })
   return lineButtonTemplate;
+}
+
+const fillStationSelector = (stationSelector) => {
+  let stations = stationStorage().getStationNameArray();
+  stations.forEach((station) => {
+    stationSelector.innerHTML += `<option>${station}</option>`;
+  })
+}
+
+const findLineObject = (lineName) => {
+  let lines = lineStorage().getLine();
+  
+  for(let i=0;i<lines.length;i++) {
+    if(lines[i].name === lineName) return lines[i];
+  }
+}
+
+const fillLineTable = (sectionTableBody, lineName) => {
+  let line = findLineObject(lineName);
+  let stations = line.stations[0];
+  console.log(sectionTableBody)
+  for(let i=0;i<stations.length;i++) {
+    let tbodyRow = sectionTableBody.insertRow(sectionTableBody.rows.length);
+    let firstCell = tbodyRow.insertCell(0);
+    let secondCell = tbodyRow.insertCell(1);
+    let thirdCell = tbodyRow.insertCell(2);
+
+    firstCell.innerHTML = i;
+    secondCell.innerHTML = stations[i];
+    thirdCell.innerHTML = '<button class="section-delete-button" data-name=' + stations[i] + '>삭제</button>';
+  }
+  
+
+  // 3호선이라고 했을때
 }
 
 export default function sectionManagerPage(contentSectionTag) {
@@ -19,13 +53,24 @@ export default function sectionManagerPage(contentSectionTag) {
   sectionSelectContainer.innerHTML = makeLineButtonView(lines);
 
   let sectionLineMenuButton = document.getElementsByClassName('section-line-menu-button');
+  // fill Register Container
   sectionSelectContainer.addEventListener('click', function(e) {
-    // console.log(e.target.dataset.linename);
-    // let lineName = e.target.dataset.linename;
+    let lineName = e.target.dataset.linename;
     sectionRegisterContainer.innerHTML = SECTION_REGISTER_FORM_TEMPLATE;
-    // let lineTitle = document.getElementById('line-title').innerText;
-    // lineTitle = lineName;
+
+    let lineTitle = document.getElementById('line-title');
+    lineTitle.innerText = lineName;
+    let stationSelector = document.getElementById('section-station-selector');
+    let lineTableContainer = document.getElementById('section-show-container');
+    
+    lineTableContainer.innerHTML = SECTION_TABLE_BODY;
+    let sectionTableBody = document.getElementsByClassName('section-table-body')[0];
+
+    fillStationSelector(stationSelector);
+    fillLineTable(sectionTableBody, lineName);
   })
+
+  // fill
 
   
 }
--- src/view/sectionTemplate.js
+++ src/view/sectionTemplate.js
@@ -4,14 +4,43 @@ const SECTION_MANAGER_PAGE_TEMPLATE =
   <nav class="line-buttons"></nav>
 </section>
 <section id="section-selector-container"></section>
-<section id="section-register-container"></section>
-<section id="section-show-container"></section>`;
+<section id="section-register-container"></section> </br>
+<section id="section-show-container">
+  
+</section>`;
+
+const SECTION_TABLE_BODY =
+`
+<table border="1">
+  <thead>
+    <tr>
+      <th>순서</th>
+      <th>이름</th>
+      <th>설정</th>
+    </tr>
+  </thead>
+  <tbody class="section-table-body">
+  </tbody>
+</table>
+`;
 
 const SECTION_REGISTER_FORM_TEMPLATE = 
-`<h3 id="line-title"></h3>
+`
+<h3 id="line-title"></h3>
 <h4>구간 등록</h4>
+<label for="section-distance">구간 거리</label>
+<input type="text" id="section-distance">
+</select>
+<br />
+<label for="duration-time">소요 시간</label>
+<input type="text" id="duration-time">
+</select>
+<br />
 <select name="start-station" id="section-station-selector"></select>
 <input type="number" id="section-order-input" placeholder="순서">
-<button id="section-add-button">등록</button>`;
+<button id="section-add-button">등록</button>
+<br />
 
-export {SECTION_MANAGER_PAGE_TEMPLATE, SECTION_REGISTER_FORM_TEMPLATE};
+`;
+
+export {SECTION_MANAGER_PAGE_TEMPLATE, SECTION_REGISTER_FORM_TEMPLATE, SECTION_TABLE_BODY};
